b84b4d94a11821f4d4984ced00cfa18aeb540463
Ciare95 <100888282+Ciare95@users.noreply.github.com>
2026-02-16 19:34:31 -0500

    Funcionalidad de CRUD de comment terminada en backend

--- comments/urls.py
+++ comments/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from comments.views import add, show_list
+from comments.views import add, show_list, get_only_comment, update_comment, delete_comment
 
 app_name = 'comments'
 
 urlpatterns = [
     path('add', add, name='add'),
     path('comments_list', show_list, name='comments_list'),
+    path('get_comment/<int:id>/', get_only_comment, name='get_only_comment'),
+    path('update_comment/<int:id>/', update_comment, name='update_comment'),
+    path('delete_comment/<int:id>/', delete_comment, name='delete_comment'),
 ]
--- comments/views.py
+++ comments/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from comments.models import Comment
 
 from comments.forms import CommentForm
@@ -9,13 +9,14 @@ def add(request):
         form = CommentForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('comments_list')
+            return redirect('comments:comments_list')
     else:
         form = CommentForm()
 
     return render(request, 'comments/add.html', {'form': form})
 
 def show_list(request):
+
     if request.method == 'GET':
         comments = Comment.objects.all()
         return render(
@@ -23,6 +24,41 @@ def show_list(request):
             'comments/comments_list.html',
             {'comments_list': comments}
         )
+
+def get_only_comment(request, id):
+
+    if request.method == 'GET':
+        get_comment = Comment.objects.get(id=id)
+        return render(
+            request,
+            'comments/comment_detail.html',
+            {'comment_detail': get_comment}
+        )
+
+def update_comment(request, id):
+
+    comment = get_object_or_404(Comment, id=id)
+
+    if request.method == 'POST':
+        form = CommentForm(request.POST, instance=comment)
+        if form.is_valid():
+            form.save()
+            return redirect('comments:comments_list')
+    else:
+        form = CommentForm(instance=comment)
+        return render(
+            request,
+            'comments/update_comment.html',
+            {'form_update': form}
+        )
+
+def delete_comment(request, id):
+
+    comment = Comment.objects.filter(id=id)
+    comment.delete()
+
+    return redirect('comments:comments_list')
+
 
 
 
